d3ba0d8da851ecaa67d39d1773d6d048391b4469
yumKing <ym1121ik8825@163.com>
2017-02-13 16:11:52 +0800

    添加用户模型的属性和方法

--- src/com/model/User.java
+++ src/com/model/User.java
@@ -2,4 +2,41 @@ package com.model;
 
 public class User {
 
+	private String userName;
+	private String password;
+	private Integer age;
+	private String sex;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public User() {
+		this.userName = "";
+		this.password = "";
+		this.age = 0;
+		this.sex = "";
+	}
+	
+	
+	
 }
